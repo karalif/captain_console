cf956b13072c1ea626181b51cf826ed9230b9d04
Arndís Einarsdóttir <arndis@Arndiss-MacBook-Pro-3.local>
2020-05-06 16:48:17 +0000

    database change

--- game/models.py
+++ game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from console.models import ConsoleCategory
+from console.models import Console
 
-# Create your models here.
+#class GameCategory(models.Model):
+#    name = models.CharField(max_length=255)
+
+class Games(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999, blank=True)
+    age_limit = models.CharField(max_length=999)
+    price = models.FloatField()
+    on_sale = models.BooleanField()
+    category = models.ForeignKey(ConsoleCategory, on_delete=models.CASCADE)
+    console = models.ForeignKey(Console, on_delete=models.CASCADE)
+
+class GameImage(models.Model):
+    image = models.CharField(max_length=999)
+    game = models.ForeignKey(Games, on_delete=models.CASCADE)
