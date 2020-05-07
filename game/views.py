808775c818f132c25189f37bcd1c3e54319e7882
Helga Lárusdóttir <helgalarusdottir@Helgas-MacBook-Pro-6.local>
2020-05-07 11:56:07 +0000

    bætti við games

--- game/views.py
+++ game/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 
-games=[
-    {'name': 'Super Mario', 'price': 4.99 },
-    {'name': 'Star Wars', 'price' : 5.99 }
-]
+from game.models import Games
 
 def index(request):
-    return render(request, 'game/index.html', context={'games': games})
+    context = {'games': Games.objects.all().order_by('name')}
+    return render(request, 'game/index.html', context)
 
 
 
