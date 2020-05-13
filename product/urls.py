847e4c7efe5c1dc2e9a32e4079851f99ec721ff2
Kara Líf Ingibergsdóttir <karalif@Karas-MacBook-Pro.local>
2020-05-13 14:09:36 +0000

    urls, css og meira

--- product/urls.py
+++ product/urls.py
@@ -4,9 +4,12 @@ urlpatterns = [
     path('', views.home_index, name="product-home_index"),
     path('games/', views.game_index,name="product-game_index"),
     path('consoles/', views.console_index,name="product-console_index"),
+    path('games/<int:id>', views.get_product_by_id, name="product_details"),
+    path('consoles/<int:id>', views.get_product_by_id, name="product_details"),
     path('<int:id>', views.get_product_by_id, name="product_details"),
     path('create_product', views.create_product, name="create_product"),
     path('delete_product/<int:id>',views.delete_product, name="delete_product"),
     path('update_product/<int:id>',views.update_product, name="update_product"),
+
 
 ]
