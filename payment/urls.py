73869e91cf01d338707b3fdc60c91120efe6ce19
Arndís Einarsdóttir <arndis@Arndiss-MacBook-Pro-3.local>
2020-05-13 14:43:56 +0000

    billing adress

--- payment/urls.py
+++ payment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('billing/', views.billing_info, name="payment-billing_address")
+    path('billing/', views.billing_info, name="payment-billing_address"),
+    path('payment_info', views.payment_info, name='payment-payment_info')
 ]
