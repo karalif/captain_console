546142c798e2b0b2e9da99cd5dc5a0abfad7b438
Katla Arnorsdottir <katlarun@Katlas-MacBook-Air-2.local>
2020-05-14 23:18:48 +0000

    bakendi fyrir Order er tilbúið

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.home_index, name="product-home_index"),
     path('products/', views.index, name="product-index"),
@@ -10,7 +11,6 @@ urlpatterns = [
     path('<int:id>', views.get_product_by_id, name="product_details"),
     path('create_product', views.create_product, name="create_product"),
     path('delete_product/<int:id>',views.delete_product, name="delete_product"),
-    path('update_product/<int:id>',views.update_product, name="update_product"),
-
+    path('update_product/<int:id>',views.update_product, name="update_product")
 
 ]
