91dc1f3a3bad2074304bb8c6b1776f8b4c964296
Arndís Einarsdóttir <arndis@Arndiss-MacBook-Pro-3.local>
2020-05-14 18:14:59 +0000

    order model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    ordered_products = models.ManyToMany(Product)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    quantity = models.DecimalField(max_digits=2, decimal_places = 0)
+    def __str__(self):
+        return self.user
